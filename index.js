f42ce76e8ab5c0c2c007a4217b6533f58ed7b40e
Kévio Castro <keviocastro@gmail.com>
2024-08-01 17:00:42 -0300

    init api send

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
@@ -15,8 +21,15 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.body == '!ping') {
-        msg.reply('pong');
+        msg.reply('xablau');
     }
+});
+
+app.get('/send', async (req, res) => {
+
+    const chatId = `${req.body.number}@c.us`;
+    const message = await client.sendMessage(chatId, req.body.message);
+    res.json({ message });
 });
 
 client.initialize();
